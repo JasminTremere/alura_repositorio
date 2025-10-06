c9031e1679efe360c6d8d22fc25178a66d088828
Yasmin Maria R. Menardo <yasminmariarmenardo@gmail.com>
2025-10-06 11:13:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("Análise de assuntos extraídos em CSV do Blip.")
 df_ead = pd.read_csv('https://raw.githubusercontent.com/JasminTremere/alura_repositorio/main/Dados_ead.csv', sep=';')
 df_pres_semi = pd.read_csv('https://raw.githubusercontent.com/JasminTremere/alura_repositorio/main/Dados_pres_semi.csv', sep=';')
 df_sup_ead = pd.read_csv('https://raw.githubusercontent.com/JasminTremere/alura_repositorio/main/Suporte_ead.csv', sep=';')
-df_sup_pres_semi = pd.read_csv('https://raw.githubusercontent.com/JasminTremere/alura_repositorio/main/Suporte_pres_semi.csv', sep=';')
+df_sup_pres_semi = pd.read_csv('https://raw.githubusercontent.com/JasminTremere/alura_repositorio/refs/heads/main/Suporte_pres_semi.csv', sep=';')
 
 
 #--------------------------------------------------------------------------------------------------------------------------------
