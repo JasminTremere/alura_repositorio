e0dbbd6d870e45ba56747ffd375aa8a495633156
Yasmin Maria R. Menardo <yasminmariarmenardo@gmail.com>
2025-10-08 07:47:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -480,7 +480,7 @@ with col_tab1:
             hide_index=True,
              column_config={
                 "Assunto": st.column_config.Column(
-                    width=125
+                    width=150
                 )
             }
         )
@@ -499,7 +499,7 @@ with col_tab2:
             hide_index=True,
             column_config={
                 "Assunto": st.column_config.Column(
-                    width=125  # Tente 150. Se ainda não funcionar, tente 100
+                    width=150  # Tente 150. Se ainda não funcionar, tente 100
                 ),
              "Telefone": st.column_config.Column(
                     width="small"
